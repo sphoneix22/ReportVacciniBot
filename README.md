2126a2156bebe624626a8c6fa3d948742f173249
sphoneix22 <simone.pugliese21@gmail.com>
2021-01-20 19:02:37 +0100

    Reading data from open data repository

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
 # Come funziona il bot
-Tramite Selenium effettua uno scraping sulla pagina del report vaccini anti COVID-19.
-Al momento il link alla pagina è [questo](https://app.powerbi.com/view?r=eyJrIjoiMzg4YmI5NDQtZDM5ZC00ZTIyLTgxN2MtOTBkMWM4MTUyYTg0IiwidCI6ImFmZDBhNzVjLTg2NzEtNGNjZS05MDYxLTJjYTBkOTJlNDIyZiIsImMiOjh9).
-
-### Dati dalle regioni
-Alcune regioni pubblicano i dati sui vaccinati prima che vengano inseriti sul report nazionale.
-Al momento il bot è in grado di recupare i dati della Regione [Toscana](https://vaccinazioni.sanita.toscana.it), [Emilia-Romagna](https://reportvaccinianticovid19er.lepida.it/), [Lazio](https://app.powerbi.com/view?r=eyJrIjoiYTg3MmEwN2QtOTQ2Zi00MzcyLWJmNTctYjkzY2E3MmE0N2RjIiwidCI6IjY0ZTY0YTY0LWZjNzMtNGIzYy05Mjc4LWFmN2I2OGQ2NjU0NCIsImMiOjl9), [Piemonte](https://www.regione.piemonte.it/web/pinforma/notizie/22203-persone-finora-vaccinate-contro-covid?fbclid=IwAR08RnwxdI-_tt-p1Ex9jO2HsSjAT-AIIwqK2YKDMsOFEooE2tt6SYegQhY).
+Il bot restuisce gli ultimi dati disponibili sulla repo [Open data vaccini](https://github.com/italia/covid19-opendata-vaccini)
 
 # Come utilizzare il bot
 Il bot è utilizzabile col tag `@reportvaccinibot` su Telegram.
@@ -14,9 +9,4 @@ Per avviarlo in locale, dopo aver installato i prerequisiti e inserito le seguen
 "TELEGRAM_TOKEN"
 "REPORT_URL"
 "PORT":"25565"
-"CHROME_EXECUTABLE_PATH"
-"URL_LAZIO"
-"URL_PIEMONTE"
-"URL_EMILIAROMAGNA"
-"URL_TOSCANA"
 ```
--- bot.py
+++ bot.py
@@ -3,13 +3,8 @@ from telegram import update
 from telegram.ext import Updater, CommandHandler
 import logging
 import os
-import datetime
-import time
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+import urllib.request as requests
+import json
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -28,125 +23,29 @@ def convert_number(number):
 
 
 def get_data(update, context):
-    last_usage = context.user_data.get('LastUse')
-    if last_usage is None or last_usage + 180 <= time.time():
-        context.user_data['LastUse'] = time.time()
-    else:
-        return context.bot.send_message(chat_id=update.effective_chat.id, text="Per favore attendi, puoi usare il comando solo ogni 3 minuti.")
-
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sto cercando i dati. Questo può richiedere fino a qualche minuto...", parse_mode=telegram.ParseMode.MARKDOWN)
     URL = os.environ.get("REPORT_URL")
 
-    option = webdriver.ChromeOptions()
-    if os.getenv("HEROKU", False):
-        option.binary_location = os.getenv("GOOGLE_CHROME_BIN")
-    option.add_argument("--headless")
-    option.add_argument('--disable-gpu')
-    option.add_argument('--no-sandbox')
-    option.add_argument("--proxy-server='direct://'")
-    option.add_argument("--proxy-bypass-list=*")
+    data = json.load(requests.urlopen(URL))['data']
 
-    driver = webdriver.Chrome(executable_path=os.getenv("CHROME_EXECUTABLE_PATH"), options=option)
-
-    driver.get(URL)
-    try:
-        #wait load page
-        d = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "title"))
-        )
-        
-        data = driver.find_elements_by_css_selector('.pivotTableCellWrap')
-
-        regioni = {}
-
-        # Da Abruzzo a Valle d'Aosta i dati sono tutti in fila, per qualche motivo quelli della Calabria sono in fondo
-        for row in range(4, 24):
-            regioni[data[row].text] = {'somministrate': data[row+21].text, 'consegnate': data[row+41].text, 'percentuale_somministrate': data[row+61].text}
-        regioni['Calabria'] = {'somministrate': data[85].text, 'consegnate': data[86].text, 'percentuale_somministrate': data[87].text} 
-
-        totale = {'somministrate': data[89].text, 'consegnate': data[90].text, 'percentuale_somministrate': data[91].text}
-
-        msg = '*Report ufficiale vaccinazioni anti COVID-19*:\n'
-
-        for regione in regioni:
-            msg += f"*{regione}*: {regioni[regione]['somministrate']} dosi somministrate di {regioni[regione]['consegnate']} consegnate ({regioni[regione]['percentuale_somministrate']})\n"
-        msg += f"*Totali*: {totale['somministrate']} dosi somministrate di {totale['consegnate']} consegnate ({totale['percentuale_somministrate']})\n\n"
-
-        msg += f"Ultimo aggiornamento: {d.text}"
-
-        context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
+    regioni = {'ABR':'Abruzzo', 'BAS':'Basilicata', 'CAL':'Calabria', 'CAM': 'Campania', 'EMR':'Emilia-Romagna', 'FVG': 'Friuli Venezia Giulia', 'LAZ': 'Lazio', 'LIG':'Liguria', 'LOM': 'Lombardia', 'MOL': 'Molise', 'MAR': 'Marche', 'PAB': 'Provincia di Bolzano', 'PAT': 'Provincia di Trento', 'PIE': 'Piemonte', 'PUG': 'Puglia', 'SAR': 'Sardegna', 'SIC': 'Sicilia', 'TOS': 'Toscana', 'UMB': 'Umbria', 'VDA': "Valle d'Aosta", 'VEN': 'Veneto'}
     
-        URL_LAZIO = os.environ.get("URL_LAZIO")
-        URL_PIEMONTE = os.environ.get("URL_PIEMONTE")
-        URL_EMILIAROMAGNA = os.environ.get("URL_EMILIAROMAGNA")
-        URL_TOSCANA = os.environ.get("URL_TOSCANA")
+    msg = "*Dati vaccinazioni anti COVID-19 in Italia:*\n"
 
-        # Lazio scraping
-        driver.get(URL_LAZIO)
-        raw_data_lazio = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".value"))
-        )
-        data_lazio = [convert_number(x.text) for x in raw_data_lazio]
-        data_lazio = max(data_lazio)
-        additional_lazio = 0
-        if data_lazio > convert_number(regioni['Lazio']['somministrate']):
-            additional_lazio = data_lazio - convert_number(regioni['Lazio']['somministrate'])
-        else:
-            additional_lazio = 0
+    totale_dosi_somministrate = 0
+    totale_dosi_consegnate = 0
+    for regione in data:
+        msg += f"*{regioni[regione['area']]}*: {regione['dosi_somministrate']} dosi somministrate di {regione['dosi_consegnate']} dosi consegnate ({regione['percentuale_somministrazione']}%)\n"
+        totale_dosi_somministrate += int(regione['dosi_somministrate'])
+        totale_dosi_consegnate += int(regione['dosi_consegnate'])
+    totale_percentuale_dosi_somministrate = round((totale_dosi_somministrate/totale_dosi_consegnate)*100, 2)
 
-        # Piemonte scraping
-        driver.get(URL_PIEMONTE)
-        raw_data_piemonte = driver.find_element_by_class_name("field--name-title").text
-        data_piemonte = ""
-        for l in raw_data_piemonte:
-            if l not in (" "):
-                data_piemonte += l
-            else:
-                break
-
-        data_piemonte = convert_number(data_piemonte)
-
-        if data_piemonte > convert_number(regioni['Piemonte']['somministrate']):
-            additional_piemonte = data_piemonte - convert_number(regioni['Piemonte']['somministrate'])
-        else:
-            additional_piemonte = 0
-
-        # ER scraping
-        driver.get(URL_EMILIAROMAGNA)
-        data_emiliaromagna = convert_number(driver.find_element_by_class_name("valore").text)
-        if data_emiliaromagna > convert_number(regioni['Emilia-Romagna']['somministrate']):
-            additional_emiliaromagna = data_emiliaromagna - convert_number(regioni['Emilia-Romagna']['somministrate'])
-        else:
-            additional_emiliaromagna = 0
-
-        # Toscana scraping
-        driver.get(URL_TOSCANA)
-        data_toscana = convert_number(WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'inner'))
-        ).find_element_by_css_selector("h3").text)
-
-        if data_toscana > convert_number(regioni['Toscana']['somministrate']):
-            addional_toscana = data_toscana - convert_number(regioni['Toscana']['somministrate'])
-        else:
-            addional_toscana = 0
-
-        total_additional = additional_lazio + additional_piemonte + additional_emiliaromagna + addional_toscana
-        if total_additional == 0:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Non ci sono ulteriori dati aggiornati da parte delle regioni.", parse_mode=telegram.ParseMode.MARKDOWN)
-        else:
-            additional_msg = f"*Stima dati aggiornati in base alle comunicazioni delle regioni:*\n{convert_number(totale['somministrate']) + total_additional} dosi somministrate\n(Questo dato potrebbe non essere accurato.)"
-            context.bot.send_message(chat_id=update.effective_chat.id, text=additional_msg, parse_mode=telegram.ParseMode.MARKDOWN)
-
-    except Exception as e:
-        print(e)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Si è verificato un errore nel recupero dei dati.")
-    finally:
-        driver.quit()
+    msg += f"*Totali*: {totale_dosi_somministrate} dosi somministrate di {totale_dosi_consegnate} dosi consegnate ({totale_percentuale_dosi_somministrate}%)"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def info(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, parse_mode=telegram.ParseMode.MARKDOWN,
-        text="Questo bot legge la pagina web del report vaccinazioni anti COVID-19 del Commissario Straordinario per l'Emergenza e quelle di alcune regioni. (Al momento Toscana, Emilia-Romagna e Piemonte)\n Creato da @Bananaglassata\n [Github](https://github.com/sphoneix22/ReportVacciniBot)")
+        text="Questo bot restituisce i dati disponibili sulla [repository Open Data Vaccini](https://github.com/italia/covid19-opendata-vaccini)\n Creato da @Bananaglassata\n [Github](https://github.com/sphoneix22/ReportVacciniBot)")
 
 def main():
     updater = Updater(token=os.environ.get("TELEGRAM_TOKEN"), use_context=True) 
